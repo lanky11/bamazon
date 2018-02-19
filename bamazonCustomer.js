1205f7c92d582e0185f40e3aed990900d3d394cb
Leif Swenson <leifswenson@leifs-mbp.lan>
2018-02-18 20:06:15 -0600

    work with inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,5 +16,30 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  //queryAllProducts();
+  start();
 });
+
+function queryAllProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i=0; i<res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name  + " | $" + res[i].price);
+    }
+    //connection.end();
+  });
+}
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "productToBuy",
+      message: "Please type the ID of the product you would like to buy?",
+    })
+    .then(function(answer) {
+      console.log("Good call, item number " + answer.productToBuy + " is a great buy!");
+      connection.end();
+    });
+}
+
