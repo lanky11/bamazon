3b7f715f35ff2bb275519aed61a3661f0c815dab
Leif Swenson <leifswenson@leifs-mbp.lan>
2018-02-18 19:14:53 -0600

    2 npm install

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+var inquirer = require('inquirer');
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
